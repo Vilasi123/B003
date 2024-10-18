1fd9df1507224221862b776ed4e74e97b672a797
Vilasi123 <kharatvilasi45@gmail.com>
2024-10-18 12:33:06 +0530

    update b class

--- src/com/sit/B.java
+++ src/com/sit/B.java
@@ -5,7 +5,7 @@ public class B {
 	public static void main(String[] args) {
 		System.out.println("b class");
 		System.out.println(" modifiess b  class");
-		
+		System.out.println(" welcome to my world");
 	}
 
 }
